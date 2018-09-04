ed16e2753b18180c93b1e9ce79f9dfa0e7f60be7
wisniad <wisniad@gmail.com>
2018-09-04 10:07:58 +0200

    Add login and sign up page

---
+++ src/components/SignUp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sign-up">
+    <p>Let's create a new account!</p>
+    <input type="text" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button>Sign up</button>
+    <span>or go back to login.</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'signUp',
+    data: function() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<!-- Global CSS -->
+<style>
+.signUp {
+    margin-top: 40px;
+}
+input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+}
+button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+}
+span {
+    margin-top: 10px;
+    width: 10%;
+    cursor: pointer;
+}
+</style>
